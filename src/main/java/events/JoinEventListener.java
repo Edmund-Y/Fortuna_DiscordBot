7ad43ff1473164170f8c296aaf001fa30215a5ba
Y_Y <85477034+Edmund-Y@users.noreply.github.com>
2022-11-10 10:29:50 +0900

    서버 접속 정보 가져오기 전 임시저

--- src/main/java/events/JoinEventListener.java
+++ src/main/java/events/JoinEventListener.java
@@ -1,19 +1,29 @@
 package events;
+import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+
 public class JoinEventListener extends ListenerAdapter {
+
+    private static Dotenv config;
+
     @Override
     public void onGuildMemberJoin(GuildMemberJoinEvent event) {
+
+        config = Dotenv.configure().load();
+        String dbuser = config.get("DBID");
+        String dbpasswd = config.get("DBPW");
+
         Guild guild = event.getGuild();
         User user = event.getUser();
         JDA client = event.getJDA();
 
         if (user.isBot() == false){
-            System.out.println("길드ID:" + guild + " 유저:" + user + "JDA:" +client );
+            System.out.println("길드ID:" + guild + " 유저닉네임:" + user.getName() + "유저ID:" + user.getId() );
 
         }
     }
